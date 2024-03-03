0b2fabf124bd7ecb5ba70a17b03444629c30ddd2
brandaoplaster <brandaoplaster@gmail.com>
2024-03-02 23:12:54 -0300

    feat: Adding libs

--- mix.exs
+++ mix.exs
@@ -49,7 +49,12 @@ defmodule AirbnbEx.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.18.0", only: :test},
+      {:sobelow, "~> 0.13.0", only: :dev},
+      {:ex_machina, "~> 2.7.0"},
+      {:faker, "~> 0.17", only: [:test, :dev]}
     ]
   end
 
