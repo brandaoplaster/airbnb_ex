d9c3b205c2a4d05486d1534d46906b6e843da10d
brandaoplaster <brandaoplaster@gmail.com>
2024-04-07 00:18:41 -0300

    fix: Fixing booking factory

--- test/support/factories/booking_factory.ex
+++ test/support/factories/booking_factory.ex
@@ -8,6 +8,9 @@ defmodule AirbnbEx.BookingFactory do
           id: Faker.UUID.v4(),
           check_in: Faker.Date.backward(30),
           check_out: Faker.Date.forward(30),
+          amount: :rand.uniform_real(),
+          quantity_days: :rand.uniform(30),
+          status: Enum.random(["active", "cancelled"]),
           property: build(:property),
           guest: build(:user)
         }
