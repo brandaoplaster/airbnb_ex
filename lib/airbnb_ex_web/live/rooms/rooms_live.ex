a4bd47e0060cb56122142c28906d2056e05da9ab
brandaoplaster <brandaoplaster@gmail.com>
2024-08-05 21:06:13 -0300

    feat: Updating rooms live

--- lib/airbnb_ex_web/live/rooms/rooms_live.ex
+++ lib/airbnb_ex_web/live/rooms/rooms_live.ex
@@ -4,6 +4,12 @@ defmodule AirbnbExWeb.Rooms.RoomsLive do
   def render(assigns) do
     ~H"""
     <div class="min-h-screen grid grid-rows-4 gap-4 p-4">
+      <.section class="bg-white p-4 shadow-md">
+        <.header class="text-left ml-9 m-4">
+          Metropolitian Studio with Cityscape, Pool and Gym
+        </.header>
+        <.banner />
+      </.section>
       <div class="bg-white p-4 shadow-md">Linha 1</div>
       <div class="bg-white p-4 shadow-md grid grid-cols-2 gap-4">
         <div class="bg-blue-100 p-4">Coluna 1 da Linha 2</div>
